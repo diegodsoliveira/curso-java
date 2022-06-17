806fe7465b0dd49504e8f478be94737ce4473f81
diego <diego@DESKTOP-P4DN8E4>
2022-06-17 16:59:08 -0300

    Criação da Classe Aluno

---
+++ curso-java-full-stack/src/aula1/Aluno.java
@@ -0,0 +1,35 @@
+package aula1;
+
+public class Aluno {
+	private String nome;
+	private float nota;
+	private String cpf;
+	private String cep;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public float getNota() {
+		return nota;
+	}
+	public void setNota(float nota) {
+		this.nota = nota;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	
+	
+}
