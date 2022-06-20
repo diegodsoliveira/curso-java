44badae75e3ee78158bfd460b1ea05f8d98cd235
diego <diego@DESKTOP-P4DN8E4>
2022-06-20 11:31:23 -0300

    Melhorando a manipulação de listas e tornando o app dinâmico.

--- curso-java-full-stack/src/aula1/aula1.java
+++ curso-java-full-stack/src/aula1/aula1.java
@@ -1,35 +1,51 @@
 package aula1;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
 public class aula1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-		Aluno aluno = new Aluno();
-		Disciplina disciplina1 = new Disciplina();
+		List<Aluno> alunos = new ArrayList<Aluno>();
 		
-		aluno.setCep("321654654");
-		aluno.setCpf("3215643132");
-		aluno.setNome("Carlos Mangueira");
+		for (int i = 1; i <= 2; i++ ) {
+			Aluno aluno = new Aluno();
+			
+			
+			/* Recebe os dados do aluno */
+			String nome = JOptionPane.showInputDialog("Informe o nome do aluno "+i+":");
+			aluno.setNome(nome);
+			/*String cep = JOptionPane.showInputDialog("Informe o cep do aluno "+i+":");
+			aluno.setCep(cep);
+			String cpf = JOptionPane.showInputDialog("Informe o cpf do aluno "+i+":");
+			aluno.setCpf(cpf);*/
+	
+			/* Recebe as disciplinas do aluno */
+			for (int qtde = 1; qtde <= 2; qtde++) {
+				Disciplina disciplina = new Disciplina();
+				String nomeDisciplina = JOptionPane.showInputDialog("Informe a disciplina "+qtde);
+				disciplina.setNomeDisciplina(nomeDisciplina);
+				String nota = JOptionPane.showInputDialog("Informe a nota da disciplina "+qtde);
+				disciplina.setNota(Float.parseFloat(nota));
+				aluno.getDisciplina().add(disciplina);
+			}
+			alunos.add(aluno);
 		
-		disciplina1.setNota(80);
-		disciplina1.setNomeDisciplina("Matematica");
-		aluno.getDisciplina().add(disciplina1);
-		
-		Disciplina disciplina2 = new Disciplina();
-		disciplina2.setNomeDisciplina("Português");
-		disciplina2.setNota(50);
-		aluno.getDisciplina().add(disciplina2);
-		
-		Disciplina disciplina3 = new Disciplina();
-		disciplina3.setNomeDisciplina("História");
-		disciplina3.setNota(92);
-		aluno.getDisciplina().add(disciplina3);
-		
-		System.out.printf("\nA soma das notas é: %f",aluno.somaNotas());
-		System.out.printf("\nA média final das notas é: %f",aluno.getMediaNota());
-		
-		System.out.println("\n" + aluno);
+		}
+		for (Aluno aluno : alunos) {
+			System.out.println("O aluno " + aluno.getNome() + " cursou as seguintes disciplinas:");
+			for (Disciplina disciplina : aluno.getDisciplina()) {
+				System.out.println("Materia: " + disciplina.getNomeDisciplina() + " nota: " + disciplina.getNota());
+			}
+			System.out.println("A soma das notas é: " + aluno.somaNotas());
+			System.out.println("A média final das notas é: " + aluno.getMediaNota());
+			System.out.println("------------------------------------");
+		}
+		System.out.println(alunos);
 		
 	}
 }
